4867a8935b0e18ea3e20e6c95f4bb1aef33df69b
Francisco Oliveira <fr.pdoliv@gmail.com>
2026-03-03 22:37:28 +0000

    chore: remvoes unused classes and functions

--- Gymbro/androidApp/src/main/kotlin/io/github/frpdoliv3/gymbro/androidApp/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package io.github.frpdoliv3.gymbro.androidApp
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
